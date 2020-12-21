463c919f74cbfb2a33264a23335eccde0019d675
Renata Kukryakova <rina.kukryakova@gmail.com>
2020-12-21 20:08:58 +0300

    kk

--- homework04/pyvcs/tree.py
+++ homework04/pyvcs/tree.py
@@ -13,12 +13,11 @@ def write_tree(gitdir: pathlib.Path, index: tp.List[GitIndexEntry], dirname: str
     """Write a tree object from the current index entries."""
     tree_entries = []
     for entry in read_index():
-        assert '/' not in entry.path, \
-            'currently only supports a single, top-level directory'
-        mode_path = '{:o} {}'.format(entry.mode, entry.path).encode()
-        tree_entry = mode_path + b'\x00' + entry.sha
+        assert "/" not in entry.path, "currently only supports a single, top-level directory"
+        mode_path = "{:o} {}".format(entry.mode, entry.path).encode()
+        tree_entry = mode_path + b"\x00" + entry.sha
         tree_entries.append(tree_entry)
-    return hash_object(b''.join(tree_entries), 'tree')
+    return hash_object(b"".join(tree_entries), "tree")
 
 
 def commit_tree(
@@ -29,28 +28,29 @@ def commit_tree(
     author: tp.Optional[str] = None,
 ) -> str:
     """Commit the current state of the index to master with given message.
-      Return hash of commit object.
-      """
+    Return hash of commit object.
+    """
     tree = write_tree()
     parent = get_local_master_hash()
     timestamp = int(time.mktime(time.localtime()))
     utc_offset = -time.timezone
-    author_time = '{} {}{:02}{:02}'.format(
+    author_time = "{} {}{:02}{:02}".format(
         timestamp,
-        '+' if utc_offset > 0 else '-',
+        "+" if utc_offset > 0 else "-",
         abs(utc_offset) // 3600,
-        (abs(utc_offset) // 60) % 60)
-    lines = ['tree ' + tree]
+        (abs(utc_offset) // 60) % 60,
+    )
+    lines = ["tree " + tree]
     if parent:
-        lines.append('parent ' + parent)
-    lines.append('author {} {}'.format(author, author_time))
-    lines.append('committer {} {}'.format(author, author_time))
-    lines.append('')
+        lines.append("parent " + parent)
+    lines.append("author {} {}".format(author, author_time))
+    lines.append("committer {} {}".format(author, author_time))
+    lines.append("")
     lines.append(message)
-    lines.append('')
-    data = '\n'.join(lines).encode()
-    sha = hash_object(data, 'commit')
-    master_path = os.path.join('.git', 'refs', 'heads', 'master')
-    write_file(master_path, (sha + '\n').encode())
-    print('committed to master: {:7}'.format(sha))
+    lines.append("")
+    data = "\n".join(lines).encode()
+    sha = hash_object(data, "commit")
+    master_path = os.path.join(".git", "refs", "heads", "master")
+    write_file(master_path, (sha + "\n").encode())
+    print("committed to master: {:7}".format(sha))
     return sha
