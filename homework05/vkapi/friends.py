c4ac57fba59484fc54ae058d8a90fff2e95be8a7
Renata Kukryakova <rina.kukryakova@gmail.com>
2021-01-04 15:38:44 +0300

    hm5

--- homework05/vkapi/friends.py
+++ homework05/vkapi/friends.py
@@ -28,7 +28,18 @@ def get_friends(
     :param fields: Список полей, которые нужно получить для каждого пользователя.
     :return: Список идентификаторов друзей пользователя или список пользователей.
     """
-    pass
+    par = {
+        "access_token": VK_CONFIG["access_token"],
+        "v": VK_CONFIG["version"],
+        "count": count,
+        "user_id": user_id if user_id is not None else "",
+        "fields": ",".join(fields) if fields is not None else "",
+        "offset": offset,
+    }
+    resp = session.get("friends.get", params=par)
+    if "error" in resp.json() or not resp.ok:
+        raise APIError(resp.json()["error"]["error_msg"])
+    return FriendsResponse(**resp.json()["response"])
 
 
 class MutualFriends(tp.TypedDict):
@@ -57,4 +68,42 @@ def get_mutual(
     :param offset: Смещение, необходимое для выборки определенного подмножества общих друзей.
     :param progress: Callback для отображения прогресса.
     """
-    pass
+    if target_uids is None:
+        par = {
+            "access_token": VK_CONFIG["access_token"],
+            "v": VK_CONFIG["version"],
+            "source_uid": source_uid if source_uid is not None else "",
+            "target_uid": target_uid,
+            "order": order,
+        }
+        resp = session.get(f"friends.getMutual", params=par)
+        if "error" in resp.json() or not resp.ok:
+            raise APIError(resp.json()["error"]["error_msg"])
+        return resp.json()["response"]
+
+    responses = []
+    if progress is None:
+        progress = lambda x: x
+    for i in progress(range(((len(target_uids) + 99) // 100))):
+        par = {
+            "access_token": VK_CONFIG["access_token"],
+            "v": VK_CONFIG["version"],
+            "target_uids": ",".join(map(str, target_uids)),
+            "order": order,
+            "count": count if count is not None else "",
+            "offset": offset + i * 100,
+        }
+        resp = session.get(f"friends.getMutual", params=par)
+        if "error" in resp.json() or not resp.ok:
+            raise APIError(resp.json()["error"]["error_msg"])
+        for j in resp.json()["response"]:
+            responses.append(
+                MutualFriends(
+                    id=j["id"],
+                    common_friends=j["common_friends"],
+                    common_count=j["common_count"],
+                )
+            )
+        if i % 3 == 2:
+            time.sleep(1)
+    return responses
