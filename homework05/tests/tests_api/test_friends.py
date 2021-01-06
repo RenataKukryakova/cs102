628acf171c7db558b6c75ff203eec1da6d4036fb
Renata Kukryakova <rina.kukryakova@gmail.com>
2021-01-06 18:22:20 +0300

    homework05

--- homework05/tests/tests_api/test_friends.py
+++ homework05/tests/tests_api/test_friends.py
@@ -1,59 +1,103 @@
-import datetime as dt
+import re
+import time
 import unittest
 
 import responses
 
-from research.age import age_predict
+from vkapi.friends import FriendsResponse, get_friends, get_mutual
 
 
-class AgeTestCase(unittest.TestCase):
-    def setUp(self):
-        self.now = dt.datetime.now()
-        self.year = self.now.year
-
+class FriendsTestCase(unittest.TestCase):
     @responses.activate
-    def test_age(self):
-        friends = [
-            {
-                "id": 1,
-                "first_name": "",
-                "last_name": "",
-                "bdate": f"01.1.{self.year-25}",
-            },
-            {
-                "id": 2,
-                "first_name": "",
-                "last_name": "",
-                "bdate": f"01.1.{self.year-20}",
-            },
-            {
-                "id": 3,
-                "first_name": "",
-                "last_name": "",
-                "bdate": f"01.1.{self.year-15}",
-            },
-            {"id": 4, "first_name": "", "last_name": "", "bdate": "9.8"},
-            {"id": 5, "first_name": "", "last_name": ""},
-        ]
+    def test_get_friends(self):
+        expected_fids = [1, 2, 3, 4, 5]
         responses.add(
             responses.GET,
             "https://api.vk.com/method/friends.get",
-            json={"response": {"count": len(friends), "items": friends}},
+            json={"response": {"count": len(expected_fids), "items": expected_fids}},
             status=200,
         )
-        self.assertEqual(20.0, age_predict(user_id=123))
+        fids = get_friends(user_id=1)
+        expected_response = FriendsResponse(count=len(expected_fids), items=expected_fids)
+        self.assertEqual(expected_response, fids)
 
     @responses.activate
-    def test_no_bdates(self):
-        friends = [
-            {"id": 1, "first_name": "", "last_name": ""},
-            {"id": 2, "first_name": "", "last_name": ""},
-            {"id": 3, "first_name": "", "last_name": ""},
-        ]
+    def test_get_mutual(self):
+        common_friends = [1, 2, 3, 4, 5]
+        target_uid = 456
         responses.add(
             responses.GET,
-            "https://api.vk.com/method/friends.get",
-            json={"response": {"count": len(friends), "items": friends}},
+            re.compile(f"https://api.vk.com/method/friends.getMutual\?.*target_uid={target_uid}.*"),
+            match_querystring=True,
+            json={"response": common_friends},
             status=200,
         )
-        self.assertIsNone(age_predict(user_id=123))
+        responses.add(
+            responses.GET,
+            re.compile(
+                f"https://api.vk.com/method/friends.getMutual\?.*target_uids={target_uid}.*"
+            ),
+            match_querystring=True,
+            json={
+                "response": [
+                    {
+                        "id": target_uid,
+                        "common_friends": common_friends,
+                        "common_count": len(common_friends),
+                    }
+                ]
+            },
+            status=200,
+        )
+        mutual_friends = get_mutual(source_uid=123, target_uid=456)
+        self.assertEqual(common_friends, mutual_friends)
+
+    @responses.activate
+    def test_get_mutual_more_than100(self):
+        responses.add(
+            responses.GET,
+            re.compile("https://api.vk.com/method/friends.getMutual\?.*offset=0.*"),
+            match_querystring=True,
+            json={"response": [{"id": 1, "common_friends": [2, 3], "common_count": 2}]},
+            status=200,
+        )
+        responses.add(
+            responses.GET,
+            re.compile("https://api.vk.com/method/friends.getMutual\?.*offset=100.*"),
+            match_querystring=True,
+            json={"response": [{"id": 2, "common_friends": [1, 3], "common_count": 2}]},
+            status=200,
+        )
+        responses.add(
+            responses.GET,
+            re.compile("https://api.vk.com/method/friends.getMutual\?.*offset=200.*"),
+            match_querystring=True,
+            json={"response": [{"id": 3, "common_friends": [1, 2], "common_count": 2}]},
+            status=200,
+        )
+
+        mutual_friends = get_mutual(target_uids=list(range(300)))
+        self.assertEqual(
+            [
+                {"common_count": 2, "common_friends": [2, 3], "id": 1},
+                {"common_count": 2, "common_friends": [1, 3], "id": 2},
+                {"common_count": 2, "common_friends": [1, 2], "id": 3},
+            ],
+            mutual_friends,
+        )
+
+    @responses.activate
+    def test_get_mutual_too_many_requests_handled_properly(self):
+        common_friends = [{"id": 1, "common_friends": [2], "common_count": 1}]
+        responses.add(
+            responses.GET,
+            "https://api.vk.com/method/friends.getMutual",
+            json={"response": common_friends},
+            status=200,
+        )
+        n_reqs = 4
+        start = time.time()
+        mutual_friends = get_mutual(target_uids=list(range(n_reqs * 100)))
+        end = time.time()
+        self.assertGreaterEqual(end - start, 1.0, msg="Слишком много запросов в секунду")
+        self.assertEqual(common_friends * n_reqs, mutual_friends)
