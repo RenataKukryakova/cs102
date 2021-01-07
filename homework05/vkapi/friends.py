7f4d908dd6ea55211b9de042c2f6ddcad85e6406
Renata Kukryakova <rina.kukryakova@gmail.com>
2021-01-07 18:30:08 +0300

    hm05

--- homework05/vkapi/friends.py
+++ homework05/vkapi/friends.py
@@ -38,6 +38,8 @@ def get_friends(
             "count": count,
             "offset": offset,
             "fields": fields,
+            "access_token": config.VK_CONFIG["access_token"],
+            "v": config.VK_CONFIG["version"],
         },
     ).json()["response"]
     return FriendsResponse(count=response["count"], items=response["items"])
