2d0a55432a3855b4b8396cc44374ac2c474a7463
Renata Kukryakova <rina.kukryakova@gmail.com>
2021-01-30 13:46:02 +0300

    exam

--- exam-winter-42/find_bugs.py
+++ exam-winter-42/find_bugs.py
@@ -1,6 +1,7 @@
 import pathlib
 
-def load_tasks(taskfile: str = "tasks.txt"):
+
+def load_tasks(taskfile: str = "C:\\Users\\Николай\\Downloads\\tasks.txt"):
     tasks = []
     path = pathlib.Path(taskfile)
     with open(path, "r", encoding="utf-8") as f:
@@ -17,14 +18,15 @@ def load_tasks(taskfile: str = "tasks.txt"):
                 current.append(tuple(node))
     return tasks
 
+
 def count_elements(graph):
     elements = [[el for el in node] for node in graph]
     unique = set([j for i in elements for j in i])
     return len(unique)
 
+
 def find_broken(graph):
     unique = count_elements(graph)
-    print(f"unique: {unique}, len(graph): {len(graph)}")
     elements = {}
     broken = 0
 
@@ -36,21 +38,41 @@ def find_broken(graph):
                 elements[element] = 0
             elements[element] += 1
 
-    print(elements)
-
     for i, count in enumerate(elements.values()):
         if i == 0:
             continue
         if count > 2:
             broken += 1
-
     return broken
+
+
+def fix(graph):
+    i = -1
+    self_ref = []
+    for link in graph:
+        i += 1
+        if link[0] == link[1]:
+            self_ref.append((link, i))
+
+    if len(self_ref) != 0:
+        return (self_ref[0][0][0], graph[self_ref[0][1] + 1][0])
+
+    if graph[i][1] != graph[0][0]:
+        graph[i] = (graph[i][0], graph[0][0])
+
+    return graph[i]
 
 
 def main():
     tasks = load_tasks()
-    print(tasks)
-    broken = find_broken(tasks[2])
-    print(broken)
+    for task in tasks:
+        broken = find_broken(task)
+        if broken == 1:
+            new = fix(task)
+            if new:
+                print(f'{new[0]} -> {new[1]}')
+            else:
+                print('V, V, V...')
+
 
 main()
